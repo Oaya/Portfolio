93fe06d26acc7c8534e7cb2ae013d3989aa9ff40
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-12 16:53:18 -0700

    add image for data

--- src/components/projectData.js
+++ src/components/projectData.js
@@ -1,8 +1,15 @@
+import Kintsugi from '../images/kintsugi.gif';
+import Scheduler from '../images/scheduler.gif'
+import Jungle from '../images/Jungle.gif';
+import Map from '../images/map.png';
+import PennyTracker from '../images/penny-tacker.png';
+import Natuor from '../images/Natour.gif'
+
 export const projectData = [
   {
     id: 1,
     title: "Kintsugi",
-    img: "",
+    img: [Kintsugi],
     description: "3 person team project to build an app that aims to promote accessibility to mental healthcare for immigrants by connection them tp providers of their cultural/linguistic background. It allows to users to book an appointment with transcribed voice messages and able to have video chatting.",
     tech: ["React", "Express.js", "socket.io", "SCSS", "PostgreSQL", 'Cypress'],
     github: "https://github.com/Oaya/kintsugi",
@@ -10,7 +17,7 @@ export const projectData = [
   {
     id: 2,
     title: "Scheduler",
-    img: "",
+    img: [Scheduler],
     description: "React scheduling application that allows users to book and cancel interview up to appointments each day.",
     tech: ['React', 'Axios', 'Node.js', "WebSockets", "PostgreSQL", 'Cypress', 'Jest', 'Storybook',],
     github: "https://github.com/Oaya/Scheduler",
@@ -18,7 +25,7 @@ export const projectData = [
   {
     id: 3,
     title: "Jungle-rails",
-    img: "",
+    img: [Jungle],
     description: "A mini e-commerce application built with Rails 4.2 for purposes of teaching Rails by example. Tested application using RSpec and Capybara.",
     tech: ['Ruby on Rails', 'PostgreSQL', 'RSpec', "Capybara"],
     github: "https://github.com/Oaya/jungle-rails",
@@ -26,7 +33,7 @@ export const projectData = [
   {
     id: 4,
     title: "Map-wiki",
-    img: "",
+    img: [Map],
     description: "3 person team project to build an app that create editable maps with pins",
     tech: ['jQuery', 'Node.js', 'Express.js', 'Google map API', 'SCSS', 'cookie-parser', 'ejs'],
     github: "https://github.com/Oaya/T2_midterm",
@@ -34,7 +41,7 @@ export const projectData = [
   {
     id: 5,
     title: "Penny-tracker",
-    img: "",
+    img: [PennyTracker],
     description: "Budget app with react to track user's expenses and incomes",
     tech: ['React', 'React-chart.js', "Firebase", 'SCSS', 'date-fns',],
     github: "https://github.com/Oaya/pennytraker",
@@ -42,7 +49,7 @@ export const projectData = [
   {
     id: 6,
     title: "Natours",
-    img: "",
+    img: [Natuor],
     description: "A responsive design website of tourist company",
     tech: ['SCSS'],
     github: "https://github.com/Oaya/Natours",
