42ae0c93d89a4455720d30cd5e56e86538f20e14
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-12 17:49:04 -0700

    add node-sass

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import Home from './components/Home';
 import About from './components/About';
 import Project from "./components/Project";
-import Contact from './components/Contact'
+import Contact from './components/Contact';
+
+import './scss/main.scss'
 
 function App() {
   return (
