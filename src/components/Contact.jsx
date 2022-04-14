1c2a1b3f4f9a708ea211cfa9763dfca57b0c093b
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-13 23:42:30 -0700

    finish up footer

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,36 @@
 import React from "react";
+import Github from "../images/icon/icons8-github-48.png";
+import LinkedIn from "../images/icon/icons8-linkedin-48.png";
+import Email from "../images/icon/icons8-mail-48.png";
 
 function Contact() {
   return (
     <div className="contact">
-      <h1>Contact Me</h1>
-      <p>
-        You can reach me via email:{" "}
-        <strong>hello@example.com</strong>
-      </p>
+      <div className="contact__link">
+        <a href="mailto: ayaaa.okzk@gmail.com">
+          <img
+            className="link__email"
+            src={Email}
+            alt="mail"
+          />
+        </a>
+        <a href="https://github.com/Oaya">
+          <img
+            className="link__github"
+            src={Github}
+            alt="github"
+          />
+        </a>
+        <a href="https://www.linkedin.com/in/aya-okizaki-3581271bb/">
+          <img
+            className="link__linkedin"
+            src={LinkedIn}
+            alt="linkedin"
+          />
+        </a>
+      </div>
+
+      <p className="copy-right">OAya © 2022.</p>
     </div>
   );
 }
