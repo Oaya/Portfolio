42f2bb45ce8f8eef1c8aac011c80b8a59f26ba6a
Aya Okizaki <74119671+Oaya@users.noreply.github.com>
2022-04-14 11:41:22 -0700

    create education

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+
 import Home from "./components/Home";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
-
-import "./scss/main.scss";
 import Navigation from "./components/Navigation";
+import Education from "./components/Education";
+import "./scss/main.scss";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Home />
       <Project />
+      <Education />
       <Contact />
     </div>
   );
